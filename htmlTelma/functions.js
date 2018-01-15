5fe7c9e3261e113f7a6a08a4b7bfe53a22be1fcf
Telma <fc44796@alunos.fc.ul.pt>
2018-01-15 13:41:48 +0000

    change trailer with screenshots

--- htmlTelma/functions.js
+++ htmlTelma/functions.js
@@ -29,28 +29,21 @@ function amountscrolled(){
 window.addEventListener("scroll", function(){
     var h = amountscrolled();
 
-    console.log(h);
-
     if(h < 33){
-    	console.log('home');
-
     	document.getElementsByClassName('active')[0].setAttribute( 'class', ' ' );
     	document.getElementById('aHome').setAttribute( 'class', 'active' );
 
     } else if ( h >= 33 && h < 66){
-    	console.log('shots');
-
-    	document.getElementsByClassName('active')[0].setAttribute( 'class', ' ' );
-    	document.getElementById('aScreenshots').setAttribute( 'class', 'active' );
-
-    } else if ( h >= 66 && h < 100){
-    	console.log('video');
 
     	document.getElementsByClassName('active')[0].setAttribute( 'class', ' ' );
     	document.getElementById('aVideo').setAttribute( 'class', 'active' );
 
+    } else if ( h >= 66 && h < 100){
+
+    	document.getElementsByClassName('active')[0].setAttribute( 'class', ' ' );
+    	document.getElementById('aScreenshots').setAttribute( 'class', 'active' );
+
     } else if ( h >= 100){
-    	console.log('about');
 
     	document.getElementsByClassName('active')[0].setAttribute( 'class', ' ' );
     	document.getElementById('aAbout').setAttribute( 'class', 'active' );
